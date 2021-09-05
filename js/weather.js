464474f2a4e3862e4d3444f40ed1c8b1e803aa3e
twillight1207 <73545327+twillight1207@users.noreply.github.com>
2021-09-06 01:46:52 +0900

    weather revised 2

--- js/weather.js
+++ js/weather.js
@@ -6,14 +6,9 @@ function onGeoOk(position) {
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`;
     fetch(url).then(response => response.json()).then(data => {
         const city = document.querySelector("#weather span:first-child");
-        const weather = document.querySelector("#weather span:nth-child(2)");
-        const temperature = document.querySelector("#weather span:last-child");
-
-        city.innerHTML = `<i class="fas fa-map-marker-alt"></i>${data.name}`;
-        
-        weather.innerText = `${data.weather[0].main} / `;
-        
-        temperature.innerHTML = `<i class="fas fa-thermometer-half"></i>${data.main.temp}`;
+        const weather = document.querySelector("#weather span:last-child");
+        city.innerHTML = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
 
